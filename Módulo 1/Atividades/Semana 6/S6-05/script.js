24747705d5031da25912eb621a9615f3064e868b
Fecef <fecef.figueiredo@gmail.com>
2022-07-05 17:11:03 -0300

    S6-06

---
+++ Módulo 1/Atividades/Semana 6/S6-05/script.js
@@ -0,0 +1,44 @@
+// Exercício 1
+const pessoa = {
+  nome: "Felipe César",
+  anoNascimento: "21/12/1992",
+  cpf: "144.987.457-60",
+  cidade: "São Bernardo do Campo",
+  estado: "São Paulo",
+  logradouro: "Rua Inês de Araújo",
+};
+
+// Exercício 2
+const escola = {
+  nome: "Colégio Expressivo",
+  cnpj: "02.975.030/0001-49",
+  areaDeAtuacao: "Educacional",
+  cidade: "Rio de Janeiro",
+  estado: "Rio de Janeiro",
+  logradouro: "Rua Eng. Lafayete Stockler",
+  curso: "Ensino Médio",
+};
+
+// Exercício 3
+const curso = {
+  nome: "Ensino Médio",
+  duracao: "3 anos",
+  turma: "2009",
+  modulos: ["Módulo 1", "Módulo 2", "Módulo 3"],
+};
+
+// Exercício 4
+const endereco = {
+  cidade: "Rio de Janeiro",
+  estado: "Rio de Janeiro",
+  logradouro: "Rua Eng. Lafayete Stockler",
+  cep: "21.070-375",
+};
+
+// Exercício 5
+const propriedades = {
+  professores: ["Gilmar", "Wallace", "Glória", "Ana Maria"],
+  alunos: ["Felipe", "Danilo", "Leo", "Prazeres", "Silva"],
+  dataInicio: 2003,
+  dataPrevistaDeFormatura: 2011,
+};
