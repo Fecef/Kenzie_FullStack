1239e49f8077f1f78ca901a882f08cf8a8726c17
Fecef <fecef.figueiredo@gmail.com>
2022-07-11 11:50:17 -0300

    S7-03 finalizada.

---
+++ Módulo 1/Atividades/Semana 7/S7-03/script.js
@@ -0,0 +1,32 @@
+// Kata 1
+const container = document.getElementById("root");
+
+// Kata 2
+const container = document.getElementById("root");
+container.innerText = "repetir";
+
+// Kata 3
+const container = document.getElementById("root");
+container.innerText = "repetir mais";
+
+// Kata 4
+const repetir = document.createElement("span");
+
+// Kata 5
+const repetir = document.createElement("span");
+repetir.innerText = "preciso repetir";
+console.log(repetir);
+
+// Kata 6
+const repetir = document.createElement("span");
+const container = document.getElementById("root");
+repetir.innerText = "repetir e repetir...";
+container.appendChild(repetir);
+
+// Kata 7
+const ul = document.createElement("ul");
+const li = document.createElement("li");
+li.innerText = "repetir cada vez mais";
+ul.appendChild(li);
+const container = document.getElementById("root");
+container.appendChild(ul);
