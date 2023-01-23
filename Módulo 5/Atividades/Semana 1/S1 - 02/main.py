ca5f13e9ec2fc4cb1fbd9d5278e30695f16ae0ca
Fecef <fecef.figueiredo@gmail.com>
2023-01-23 17:37:29 -0300

    Tarefa finalizada.

---
+++ Módulo 5/Atividades/Semana 1/S1 - 02/main.py
@@ -0,0 +1,34 @@
+def corresponding_parenthesis(str):
+    abertos = str.count("(")
+    fechados = str.count(")")
+    total = abertos - fechados
+    result = ""
+    parenteses = ""
+
+    if total > 0:
+        parenteses = ")"
+        result += parenteses * abs(total)
+    else:
+        parenteses = "("
+        result += parenteses * abs(total)
+
+    return result
+
+
+print(corresponding_parenthesis("()()"))
+print(corresponding_parenthesis("()))"))
+print(corresponding_parenthesis(")))((((("))
+
+
+def remove_more_than_two_repetitions(str):
+    result = str[:2]
+
+    for char in str[2:]:
+        if result[-1] and result[-2] != char:
+            result += char
+
+    return result
+
+
+print(remove_more_than_two_repetitions(
+    "Ollloco meuuuu, taaa peegaando fogoo biiiiichooo"))
