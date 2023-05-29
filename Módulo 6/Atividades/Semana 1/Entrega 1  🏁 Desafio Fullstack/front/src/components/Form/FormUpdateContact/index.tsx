02d216672c410fe18f8a2d240b13aa1d15f4dbc7
Fecef <fecef.figueiredo@gmail.com>
2023-05-29 12:32:50 -0300

    style: minor spelling change

--- Módulo 6/Atividades/Semana 1/Entrega 1  🏁 Desafio Fullstack/front/src/components/Form/FormUpdateContact/index.tsx
+++ Módulo 6/Atividades/Semana 1/Entrega 1  🏁 Desafio Fullstack/front/src/components/Form/FormUpdateContact/index.tsx
@@ -9,41 +9,67 @@ import { Props } from "@/pages/contact/[id]";
 import { useContact } from "@/contexts/contactContext";
 
 const FormUpdateContact = ({ contact, id }: Props) => {
-    const { updateContact, removeContact } = useContact();
-    const { register, handleSubmit, formState: { errors } } = useForm<IContactPerfil>({ resolver: yupResolver(schemaFormUpdateContact) })
+  const { updateContact, removeContact } = useContact();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IContactPerfil>({
+    resolver: yupResolver(schemaFormUpdateContact),
+  });
 
-    const formSubmit = (data: IContactPerfil) => {
-        const sanitaziedData = pickBy(data, value => value!.length > 0);
-        updateContact(id, sanitaziedData)
-    };
+  const formSubmit = (data: IContactPerfil) => {
+    const sanitaziedData = pickBy(data, (value) => value!.length > 0);
+    updateContact(id, sanitaziedData);
+  };
 
-    const handleDelete = () => {
-        removeContact(id)
-    }
+  const handleDelete = () => {
+    removeContact(id);
+  };
 
-    return (
-        <Box onSubmit={handleSubmit(formSubmit)}>
-            <h2>ALTERAR CONTATO</h2>
+  return (
+    <Box onSubmit={handleSubmit(formSubmit)}>
+      <h2>ALTERAR CONTATO</h2>
 
-            <label>
-                Nome Completo
-                <input type="text" id="name" autoComplete="off" placeholder={contact.name} {...register("name")} />
-            </label>
+      <label>
+        Nome Completo
+        <input
+          type="text"
+          id="name"
+          autoComplete="off"
+          placeholder={contact.name}
+          {...register("name")}
+        />
+      </label>
 
-            <label>
-                Email
-                <input type="email" id="email" autoComplete="off" placeholder={contact.email} {...register("email")} />
-            </label>
+      <label>
+        Email
+        <input
+          type="email"
+          id="email"
+          autoComplete="off"
+          placeholder={contact.email}
+          {...register("email")}
+        />
+      </label>
 
-            <label>
-                Celular
-                <input type="tel" id="phone" autoComplete="off" placeholder={contact.phone} {...register("phone")} />
-            </label>
+      <label>
+        Celular
+        <input
+          type="tel"
+          id="phone"
+          autoComplete="off"
+          placeholder={contact.phone}
+          {...register("phone")}
+        />
+      </label>
 
-            <button type="submit">Alterar</button>
-            <button className="delete" type="button" onClick={() => handleDelete()}>Encerrar Conta</button>
-        </Box>
-    )
-}
+      <button type="submit">Alterar</button>
+      <button className="delete" type="button" onClick={() => handleDelete()}>
+        Excluir Contato
+      </button>
+    </Box>
+  );
+};
 
 export default FormUpdateContact;
